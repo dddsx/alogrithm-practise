ef9704f06f3cc6a19f8bc9b59ce5ef9926c9181e
lvxiangjun <lvxiangjun@able-elec.com>
2019-05-22 15:57:23 +0800

    IntegerToRoman_12

---
+++ LeetCodePractise/src/string/IntegerToRoman_12.java
@@ -0,0 +1,98 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IntegerToRoman_12 {
+    
+    public static void main(String[] args) {
+        System.out.println(new Solution().intToRoman(1994));
+    }
+    
+    static class Solution {
+    
+        static char I = 'I';
+        static char V = 'V';
+        static char X = 'X';
+        static char L = 'L';
+        static char C = 'C';
+        static char D = 'D';
+        static char M = 'M';
+        
+        static Map<Integer, Character> romanNums = new HashMap<>();
+        static {
+            romanNums.put(1, I);
+            romanNums.put(5, V);
+            romanNums.put(10, X);
+            romanNums.put(50, L);
+            romanNums.put(100, C);
+            romanNums.put(500, D);
+            romanNums.put(1000, M);
+        }
+        
+        public String intToRoman(int num) {
+            StringBuilder result = new StringBuilder();
+            
+            int thousandNum = num / 1000;
+            while (thousandNum-- > 0) {
+                result.append(romanNums.get(1000));
+            }
+            
+            num = num % 1000;
+            int hundredNum = num / 100;
+            if (hundredNum > 0) {
+                if (hundredNum == 9) {
+                    result.append(C).append(M);
+                } else if (hundredNum == 4) {
+                    result.append(C).append(D);
+                } else {
+                    if (hundredNum >= 5) {
+                        result.append(romanNums.get(500));
+                        hundredNum = hundredNum - 5;
+                    }
+                    while (hundredNum-- > 0) {
+                        result.append(romanNums.get(100));
+                    }
+                }
+            }
+            
+            num = num % 100;
+            int decadeNum = num / 10;
+            if (decadeNum > 0) {
+                if (decadeNum == 9) {
+                    result.append(X).append(C);
+                } else if (decadeNum == 4) {
+                    result.append(X).append(L);
+                } else {
+                    if (decadeNum >= 5) {
+                        result.append(romanNums.get(50));
+                        decadeNum = decadeNum - 5;
+                    }
+                    while (decadeNum-- > 0) {
+                        result.append(romanNums.get(10));
+                    }
+                }
+            }
+    
+            num = num % 10;
+            int unitNum = num;
+            if (unitNum > 0) {
+                if (unitNum == 9) {
+                    result.append(I).append(X);
+                } else if (unitNum == 4) {
+                    result.append(I).append(V);
+                } else {
+                    if (unitNum >= 5) {
+                        result.append(romanNums.get(5));
+                        unitNum = unitNum - 5;
+                    }
+                    while (unitNum-- > 0) {
+                        result.append(romanNums.get(1));
+                    }
+                }
+            }
+            
+            return result.toString();
+        }
+    }
+}
--- LeetCodePractise/src/string/PhoneNumber_17.java
+++ LeetCodePractise/src/string/PhoneNumber_17.java
@@ -6,49 +6,48 @@ import java.util.List;
 import java.util.Map;
 
 public class PhoneNumber_17 {
-
-}
-
-class Solution {
     
-    static Map<String, String> phone = new HashMap<>();
-    static {
-        phone.put("2", "abc");
-        phone.put("3", "def");
-        phone.put("4", "ghi");
-        phone.put("5", "jkl");
-        phone.put("6", "mno");
-        phone.put("7", "pqrs");
-        phone.put("8", "tuv");
-        phone.put("9", "wxyz");
-    }
+    static class Solution {
     
-    List<String> output = new ArrayList<>();
-    
-    public void backtrack(String combination, String next_digits) {
-        // if there is no more digits to check
-        if (next_digits.length() == 0) {
-            // the combination is done
-            output.add(combination);
+        static Map<String, String> phone = new HashMap<>();
+        static {
+            phone.put("2", "abc");
+            phone.put("3", "def");
+            phone.put("4", "ghi");
+            phone.put("5", "jkl");
+            phone.put("6", "mno");
+            phone.put("7", "pqrs");
+            phone.put("8", "tuv");
+            phone.put("9", "wxyz");
         }
-        // if there are still digits to check
-        else {
-            // iterate over all letters which map
-            // the next available digit
-            String digit = next_digits.substring(0, 1);
-            String letters = phone.get(digit);
-            for (int i = 0; i < letters.length(); i++) {
-                String letter = phone.get(digit).substring(i, i + 1);
-                // append the current letter to the combination
-                // and proceed to the next digits
-                backtrack(combination + letter, next_digits.substring(1));
+    
+        List<String> output = new ArrayList<>();
+    
+        public void backtrack(String combination, String next_digits) {
+            // if there is no more digits to check
+            if (next_digits.length() == 0) {
+                // the combination is done
+                output.add(combination);
+            }
+            // if there are still digits to check
+            else {
+                // iterate over all letters which map
+                // the next available digit
+                String digit = next_digits.substring(0, 1);
+                String letters = phone.get(digit);
+                for (int i = 0; i < letters.length(); i++) {
+                    String letter = phone.get(digit).substring(i, i + 1);
+                    // append the current letter to the combination
+                    // and proceed to the next digits
+                    backtrack(combination + letter, next_digits.substring(1));
+                }
             }
         }
-    }
     
-    public List<String> letterCombinations(String digits) {
-        if (digits.length() != 0)
-            backtrack("", digits);
-        return output;
+        public List<String> letterCombinations(String digits) {
+            if (digits.length() != 0)
+                backtrack("", digits);
+            return output;
+        }
     }
 }
